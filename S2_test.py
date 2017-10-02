f695e62b809c0f935cc23c0e83600f372e5b2c47
Adrien Roussel <Roussel-adrien@outlook.fr>
2017-10-02 11:55:44 +0200

    Convert the import to a function

--- S2_test.py
+++ S2_test.py
@@ -1,9 +1,20 @@
 import pytest
-import asssignments.Session1.S1_algotools as algo
+import 
+def load_S1_script():
+    """
+        utility function that tris to load the script written along the first lesson
+        @throws an ImportError exception if the script file does not exist
+        @return the script as a loaded module
+    """
+    S1_script_filename='assignments/Session1/S1_algotools.py'
+    import imp
+    s1_algotools=imp.load_source('session_1_script', S1_script_filename)
+    return  s1_algotools
+
 
 input_list = [1,2,3,4]
 
 def test_average_above_list():
-    assert algo.average_above_zero(input_list)==2.5 
+    assert load_S1_script().average_above_zero(input_list)==2.5 
     
     
