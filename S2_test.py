5d3c107151d7af939764a5df2274006a2ae1dcc4
Adrien Roussel <Roussel-adrien@outlook.fr>
2017-10-03 09:31:31 +0200

    Put inputs in each unity test

--- S2_test.py
+++ S2_test.py
@@ -11,26 +11,26 @@ def load_S1_script():
     return  s1_algotools
 
 
-load_S1_script()
-
-input_list = [1,2,3,4]
-my_list = [10, 15, 7, 1, 3, 3, 9]
-mystring = "Hello world"
-
 def test_average_above_list():
+    input_list = [1,2,3,4]
     assert load_S1_script().average_above_zero(input_list)== 2.5 
     
 def test_max_value():
+    input_list = [1,2,3,4]
     assert load_S1_script().max_value(input_list)== (4, 3) 
 
 def test_reverse_table():
+    input_list = [1,2,3,4]
     assert load_S1_script().reverse_table(input_list)== [4, 3, 2, 1]
     
 def test_remove_whitespace():
+    mystring = "Hello world"
     assert load_S1_script().remove_whitespace(mystring)== "Helloworld"
 
 def test_sort_selective():
+    my_list = [10, 15, 7, 1, 3, 3, 9]
     assert load_S1_script().sort_selective(my_list)== [1, 3, 3, 7, 9, 10, 15]
 
 def test_sort_bubble():
+    my_list = [10, 15, 7, 1, 3, 3, 9]
     assert load_S1_script().sort_bubble(my_list)== [1, 3, 3, 7, 9, 10, 15]
